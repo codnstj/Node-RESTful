46b209c25b7aa02a15257c47ab131be5d628b14d
lot8229 <lot8229@kakao.com>
2021-10-28 19:17:00 +0900

    [ADD] Web-Server

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// @ts-check
+
+// 프레임워크 없이 간단한 토이 프로젝트 웹 서버 만들어보기
+
+/**
+ * 블로그 포스팅 서비스
+ *  - 로컬 파일을 데이터베이스로 활용할 예정 (JSON)
+ *  - 인증 로직은 넣지 않습니다.
+ *  - RESTful API 를 사용합니다.
+ */
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.end('hello!')
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log(`the server is listen on ${PORT}`)
+})
