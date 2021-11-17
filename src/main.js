bc92c5bd0d9663716a392a1301d17577e909b128
lot8229 <lot8229@kakao.com>
2021-11-17 17:34:57 +0900

    [ADD] jsondoc 적용

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // @ts-check
 
 // 프레임워크 없이 간단한 토이 프로젝트 웹 서버 만들어보기
@@ -12,6 +13,32 @@
 const http = require('http')
 
 /**
+ * jsdoc : 함수가 어떤 파라미터를 받고 어떤걸 접근 하는지 주석으로 나타내는 것
+ */
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+/** @type {Post[]} */
+// eslint-disable-next-line no-unused-vars
+
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My first post',
+    content: 'hello!',
+  },
+  {
+    id: 'my_second_post',
+    title: '나의 두번째 게시물',
+    content: 'Second Post',
+  },
+]
+
+/**
  * Post
  *
  * GET /posts
@@ -19,11 +46,40 @@ const http = require('http')
  * POST /posts
  */
 const server = http.createServer((req, res) => {
-  console.log(req.url)
-  console.log('Requst accepted!')
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-z0-9-_]+)$/
+  const PostIdRegexResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+  if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      tottalCount: posts.length,
+    }
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(JSON.stringify(result))
+  } else if (PostIdRegexResult) {
+    // GET /posts/:id
+    const postId = PostIdRegexResult[1]
 
-  res.statusCode = 200
-  res.end('hello!')
+    const post = posts.find((_post) => _post.id === postId)
+    if (post) {
+      res.statusCode = 200
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Post NotFOund')
+    }
+    console.log(`postId : ${postId}`)
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('Creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not found.')
+  }
 })
 
 const PORT = 4000
